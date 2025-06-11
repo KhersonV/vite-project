48a6d5531165455ee52155f30e03247e2c851659
KhersonV <khersonv110990@gmail.com>
2025-06-11 23:37:48 +0200

    making magic with logo

--- src/App.jsx
+++ src/App.jsx
@@ -1,38 +1,24 @@
-import { useState } from 'react'
+import  ReactIcon  from './assets/react.svg?component'
 import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   // декларативный
   const currentYear = new Date().getFullYear()
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div className="App">
+      <div className="logos">
+        {/* 1. React-logo как компонент */}
+        <ReactIcon className="logo-component"/>
+
+        {/* 2. React-logo как обычное <img> */}
+        <img src={reactLogo} className="logo-img" alt="React logo" />
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-	    <p>Learn React — © {currentYear}</p>
-    </>
+      <footer>
+        <p>© {currentYear}</p>
+      </footer>
+    </div>
   )
 }
 
