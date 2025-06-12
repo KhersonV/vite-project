b41781d1f723145f7042270381aeec03dd9a810d
KhersonV <khersonv110990@gmail.com>
2025-06-12 20:26:43 +0200

    added onInputButtonClick

--- src/App.jsx
+++ src/App.jsx
@@ -4,16 +4,31 @@ import './App.css';
 
 function App() {
 
+  const [value, setValue] = useState("");
+  const [newValue, setNewValue] = useState("");
+  const [list, setList] = useState([]);
+  const [error, setError] = useState(false);
+
+ const onInputButtonClick = () => {
+   const input = prompt('Введите значение');
+    if (input !== null) {
+      setNewValue(input);
+      setError(input.length < 3);
+      console.log('input:', input);
+    }
+ };
+
+
 
   return (
     <div className="app">
     <h1 className="page-heading">Ввод значения</h1>
     <p className="no-margin-text">
-      Текущее значение <code>value</code>: "<output className="current-value"></output>"
+      Текущее значение <code>value</code>: "<output className="current-value">{newValue}</output>"
     </p>
     <div className="error">Введенное значение должно содержать минимум 3 символа</div>
     <div className="buttons-container">
-      <button className="button">Ввести новое</button>
+      <button className="button" onClick={onInputButtonClick}>Ввести новое</button>
       <button className="button" disabled>Добавить в список</button>
     </div>
     <div className="list-container">
