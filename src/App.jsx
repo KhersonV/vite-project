801108478466c9bb17104acee7a4b06efa055ec9
KhersonV <khersonv110990@gmail.com>
2025-06-13 14:16:26 +0200

    initial commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,61 @@
-import ReactIcon from './assets/react.svg?component';
-import reactLogo from './assets/react.svg';
-import './App.css';
+import styles from './app.module.css';
+import data from './data.json';
 
-function App() {
-	// декларативный
-	const currentYear = new Date().getFullYear();
+export const App = () => {
+	// Можно задать 2 состояния — steps и activeIndex
+
+	// И определить 3 обработчика: Клик назад, Клик вперед, Начать сначала
+
+	// И 2 переменных-флага — находимся ли мы на первом шаге, и находимся ли на последнем
 
 	return (
-		<div className="App">
-			<div className="logos">
-				{/* 1. React-logo как компонент */}
-				<ReactIcon className="logo-component" />
-
-				{/* 2. React-logo как обычное <img> */}
-				<img src={reactLogo} className="logo-img" alt="React logo" />
+		<div className={styles.container}>
+			<div className={styles.card}>
+				<h1>Инструкция по готовке пельменей</h1>
+				<div className={styles.steps}>
+					<div className={styles['steps-content']}>
+						{/* Для получения активного контента использйте steps и activeIndex */}
+						Контент соответственный шагу. Сейчас активен шаг 3
+					</div>
+					<ul className={styles['steps-list']}>
+						{/* Выводите <li> с помощью массива steps и метода map(), подставляя в разметку нужные значения и классы */}
+						<li className={styles['steps-item'] + ' ' + styles.done}>
+							{/* Для того, чтобы вычислить необходимый класс используйте активный индекс, текущий индекс, а также тернарные операторы */}
+							<button className={styles['steps-item-button']}>1</button>
+							{/* При клике на кнопку установка выбранного шага в качестве активного */}
+							Шаг 1
+						</li>
+						<li className={styles['steps-item'] + ' ' + styles.done}>
+							<button className={styles['steps-item-button']}>2</button>
+							Шаг 2
+						</li>
+						<li
+							className={
+								styles['steps-item'] +
+								' ' +
+								styles.done +
+								' ' +
+								styles.active
+							}
+						>
+							<button className={styles['steps-item-button']}>3</button>
+							Шаг 3
+						</li>
+						<li className={styles['steps-item']}>
+							<button className={styles['steps-item-button']}>4</button>
+							Шаг 4
+						</li>
+					</ul>
+					<div className={styles['buttons-container']}>
+						<button className={styles.button}>Назад</button>
+						<button className={styles.button}>
+							Далее
+							{/* "Начать сначала", можно сделать этой же кнопкой, просто подменять обработчик и текст в зависимости от условия */}
+							{/* Или заменять всю кнопку в зависимости от условия */}
+						</button>
+					</div>
+				</div>
 			</div>
-			<footer>
-				<p>© {currentYear}</p>
-			</footer>
 		</div>
 	);
-}
-
-export default App;
+};
--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,10 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
-import AppNoJSX from "./App.nojsx.js"
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import './index.css';
+import { App } from './app.jsx';
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    {/* <App /> */}
-    <AppNoJSX></AppNoJSX>
-  </StrictMode>,
-)
+	<StrictMode>
+		<App />
+	</StrictMode>,
+);
