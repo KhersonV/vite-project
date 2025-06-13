fb5e76afebcb1e994f405ef3aa74781a0a7b8838
KhersonV <khersonv110990@gmail.com>
2025-06-13 11:38:00 +0200

    added prettier and some work on handlers

--- src/App.jsx
+++ src/App.jsx
@@ -1,45 +1,49 @@
 import React, { useState } from 'react';
 import './App.css';
 
-
 function App() {
+	const [isValueVaild, setValue] = useState(false);
+	const [newValue, setNewValue] = useState('');
+	const [list, setList] = useState([]);
+	const [error, setError] = useState('');
 
-  const [value, setValue] = useState("");
-  const [newValue, setNewValue] = useState("");
-  const [list, setList] = useState([]);
-  const [error, setError] = useState(false);
+	const onInputButtonClick = () => {
+		setError('');
+		const promptValue = prompt('Введите значение');
+		if (promptValue !== null) {
+			setNewValue(promptValue);
+			if (promptValue.length < 3)
+				setError('Введенное значение должно содержать минимум 3 символа');
+		}
+	};
 
- const onInputButtonClick = () => {
-   const input = prompt('Введите значение');
-    if (input !== null) {
-      setNewValue(input);
-      setError(input.length < 3);
-      console.log('input:', input);
-    }
- };
+	return (
+		<div className="app">
+			<h1 className="page-heading">Ввод значения</h1>
+			<p className="no-margin-text">
+				Текущее значение <code>value</code>: "
+				<output className="current-value">{newValue}</output>"
+			</p>
 
+			{error && <div className="error">{error}</div>}
 
-
-  return (
-    <div className="app">
-    <h1 className="page-heading">Ввод значения</h1>
-    <p className="no-margin-text">
-      Текущее значение <code>value</code>: "<output className="current-value">{newValue}</output>"
-    </p>
-    <div className="error">Введенное значение должно содержать минимум 3 символа</div>
-    <div className="buttons-container">
-      <button className="button" onClick={onInputButtonClick}>Ввести новое</button>
-      <button className="button" disabled>Добавить в список</button>
-    </div>
-    <div className="list-container">
-      <h2 className="list-heading">Список:</h2>
-      <p className="no-margin-text">Нет добавленных элементов</p>
-      <ul className="list">
-        <li className="list-item">Первый элемент</li>
-      </ul>
-    </div>
-  </div>
-  );
+			<div className="buttons-container">
+				<button className="button" onClick={onInputButtonClick}>
+					Ввести новое
+				</button>
+				<button className="button" disabled={!isValueVaild}>
+					Добавить в список
+				</button>
+			</div>
+			<div className="list-container">
+				<h2 className="list-heading">Список:</h2>
+				<p className="no-margin-text">Нет добавленных элементов</p>
+				<ul className="list">
+					<li className="list-item">Первый элемент</li>
+				</ul>
+			</div>
+		</div>
+	);
 }
 
 export default App;
