7a28b227da2bd634efb5ea2b73b022004ba351a7
KhersonV <khersonv110990@gmail.com>
2025-06-13 11:40:44 +0200

    added prettier

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,25 @@
-import  ReactIcon  from './assets/react.svg?component'
-import reactLogo from './assets/react.svg'
-import './App.css'
+import ReactIcon from './assets/react.svg?component';
+import reactLogo from './assets/react.svg';
+import './App.css';
 
 function App() {
-  // декларативный
-  const currentYear = new Date().getFullYear()
+	// декларативный
+	const currentYear = new Date().getFullYear();
 
-  return (
-    <div className="App">
-      <div className="logos">
-        {/* 1. React-logo как компонент */}
-        <ReactIcon className="logo-component"/>
+	return (
+		<div className="App">
+			<div className="logos">
+				{/* 1. React-logo как компонент */}
+				<ReactIcon className="logo-component" />
 
-        {/* 2. React-logo как обычное <img> */}
-        <img src={reactLogo} className="logo-img" alt="React logo" />
-      </div>
-      <footer>
-        <p>© {currentYear}</p>
-      </footer>
-    </div>
-  )
+				{/* 2. React-logo как обычное <img> */}
+				<img src={reactLogo} className="logo-img" alt="React logo" />
+			</div>
+			<footer>
+				<p>© {currentYear}</p>
+			</footer>
+		</div>
+	);
 }
 
-export default App
+export default App;
